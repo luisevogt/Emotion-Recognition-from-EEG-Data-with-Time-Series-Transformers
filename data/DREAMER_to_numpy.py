eef85bbaf19ba81928fb810b756790852d424e41
luisevogt <luise.vogt@student.maastrichtuniversity.nl>
2023-02-06 13:37:15 +0100

    :monocle_face: Update conversion to numpy array with padding

--- data/DREAMER_to_numpy.py
+++ data/DREAMER_to_numpy.py
@@ -1,74 +1,73 @@
 import scipy.io
 import numpy as np
 import pandas as pd
+import os
 
 
 def main():
     """
     small script to convert DREAMER dataset to numpy array and pandas dataframe
     """
+    file_dir = os.path.dirname(__file__)
+    dreamer_path = os.path.join(file_dir, 'DREAMER.mat')
 
-    mat = scipy.io.loadmat('DREAMER.mat')
+    mat = scipy.io.loadmat(dreamer_path)
     data = mat['DREAMER']
     dreamer = data[0, 0]['Data']
-    print(dreamer.shape)
 
-    # flattened data array
-    # 23x18 = 414 samples
-    # 14 channels
-    # 7808 values
-    numpy_data_array = np.zeros((414, 14, 7808))
-    # numpy_baseline_array = np.zeros((414, 14, 25472))
+    # convert to numpy arrays
+    max_length = 50432
+    numpy_data_array = np.full((414, 14, max_length), np.NINF)
+    numpy_baseline_array = np.zeros((414, 14, 7808))
     numpy_label_array = np.zeros((414, 3))
 
-    cols = ['Age', 'Gender', 'EEG', 'Valence', 'Arousal', 'Dominance']
-    df = pd.DataFrame(columns=cols)
-
-    # loop over dreamer dataset
+    # save video lengths
+    col = ['sample length']
+    df = pd.DataFrame(columns=col)
     position_counter = 0
+    # loop over dreamer dataset
     for participant in dreamer[0]:
         # len(participant_data) = 7: Age, Gender, EEG, ECG, Valence, Arousal, Dominance
         participant_data = participant[0, 0]
 
-        df_row = []
         for index, data_value in enumerate(participant_data):
-            if index == 0 or index == 1:
-                df_row.append(data_value[0])
-            elif index == 3:
-                # drop ECG
+            if index == 0 or index == 1 or index == 3:
                 continue
             elif index == 2:
-                eeg_stimuli = data_value[0, 0][0]
-                eeg_baseline = data_value[0, 0][1]
+                eeg_baseline = data_value[0, 0][0]
+                eeg_stimuli = data_value[0, 0][1]
 
-                tmp = []
                 for baseline_measurement, video_measurement in zip(eeg_baseline, eeg_stimuli):
                     # swap axes
-                    # baseline_array = np.swapaxes(baseline_measurement[0], 0, 1)
+                    baseline_array = np.swapaxes(baseline_measurement[0], 0, 1)
                     stimuli_array = np.swapaxes(video_measurement[0], 0, 1)
 
                     # write in final array
-                    numpy_data_array[position_counter] = stimuli_array
-                    # numpy_baseline_array[position_counter] = baseline_array
-                    tmp.append(stimuli_array)
+                    numpy_baseline_array[position_counter] = baseline_array
 
-                df_row.append(tmp)
+                    video_length = stimuli_array.shape[1]
+                    npad = ((0, 0), (0, max_length - video_length))
+                    padded_array = np.pad(stimuli_array, pad_width=npad, mode='constant', constant_values=np.NINF)
+                    numpy_data_array[position_counter] = padded_array
+
+                    # store video length in sample size
+                    if position_counter < 18:
+                        tmp = pd.DataFrame([[video_length]], columns=col)
+                        df = pd.concat([df, tmp], ignore_index=True)
+
+                    position_counter += 1
+
             else:
                 idx_to_labelidx = {4: 0, 5: 1, 6: 2}
-                numpy_label_array[position_counter:position_counter + 18, idx_to_labelidx[index]] = data_value.flatten()
-                df_row.append(data_value.flatten())
+                numpy_label_array[position_counter - 18:position_counter, idx_to_labelidx[index]] = data_value.flatten()
 
-            position_counter += 1
+    # save array
+    np.save(os.path.join(file_dir, 'DREAMER_data.npy'), numpy_data_array)
+    np.save(os.path.join(file_dir, 'DREAMER_baseline.npy'), numpy_baseline_array)
+    np.save(os.path.join(file_dir, 'DREAMER_labels.npy'), numpy_label_array)
 
-        # append row
-        tmp_df = pd.DataFrame([df_row], columns=cols)
-        df = pd.concat([df, tmp_df], ignore_index=True)
-        position_counter += 1
-
-    # save array and dataframe
-    df.to_csv('DREAMER_dataframe.csv')
-    np.save('DREAMER_data.npy', numpy_data_array)
-    np.save('DREAMER_labels.npy', numpy_label_array)
+    # save video lengths
+    df.to_csv(os.path.join(file_dir, 'DREAMER_#samples_per_video.csv'))
 
 
 if __name__ == "__main__":

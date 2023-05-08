78cc69ce5d6c368b1ef367be4c714518b16ae83c
luisevogt <luise.vogt@student.maastrichtuniversity.nl>
2023-05-08 16:24:06 +0200

    classification with one layer only

--- model/classifier/BinaryClassifier.py
+++ model/classifier/BinaryClassifier.py
@@ -11,26 +11,28 @@ class BinaryClassifier(nn.Module):
 
         self._reduced_dim = reduced_dim
 
-        self.layer_1 = nn.Linear(hidden_dim, self._reduced_dim)
-        self.layer_2 = nn.Linear(self._reduced_dim, self._reduced_dim)
-        self.layer_out = nn.Linear(self._reduced_dim, 1)
+        self.layer_1 = nn.Linear(hidden_dim, 1)
+        # self.layer_2 = nn.Linear(self._reduced_dim, self._reduced_dim)
+        # self.layer_out = nn.Linear(self._reduced_dim, 1)
 
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=dropout)
+        # self.relu = nn.ReLU()
+        # self.dropout = nn.Dropout(p=dropout)
 
-        self.batchnorm1 = nn.BatchNorm1d(64)
-        self.batchnorm2 = nn.BatchNorm1d(self._reduced_dim)
+        # self.batchnorm1 = nn.BatchNorm1d(64)
+        # self.batchnorm2 = nn.BatchNorm1d(self._reduced_dim)
 
     def forward(self, inputs):
         # extract cls token and take the average
         cls_tokens = inputs[:, :, 0, :]
         avg_token = torch.mean(cls_tokens, dim=1)
 
-        x = self.relu(self.layer_1(avg_token))
-        x = self.batchnorm1(x)
-        x = self.relu(self.layer_2(x))
-        x = self.batchnorm2(x)
-        x = self.dropout(x)
-        x = self.layer_out(x)
+        x = self.layer_1(avg_token)
+
+        # x = self.relu(self.layer_1(avg_token))
+        # x = self.batchnorm1(x)
+        # x = self.relu(self.layer_2(x))
+        # x = self.batchnorm2(x)
+        # x = self.dropout(x)
+        # x = self.layer_out(x)
 
         return x
